25cbdeb56f21e8223e31320b1e0ac3d8f1787f42
Janaka Peiris <janaka.peiris@chamonix.com.au>
2024-01-05 01:11:54 +1030

    fixed search bar

--- docs/admin_scripts/check_services.sh
+++ docs/admin_scripts/check_services.sh
@@ -1,7 +1,7 @@
 systemctl status nginx
 #sudo nginx -t
 #sudo systemctl restart nginx
-
+ 
 sudo systemctl status gunicorn.service
 #sudo systemctl status gunicorn
 #sudo journalctl -u gunicorn -n 100
